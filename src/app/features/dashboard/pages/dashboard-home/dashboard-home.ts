41bf518cccf827478f2b1a7149910cb155cf90c3
zh-deng <heromemo@web.de>
2025-09-19 22:55:09 +0200

    add dashboard home page component

---
+++ src/app/features/dashboard/pages/dashboard-home/dashboard-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-home',
+  imports: [],
+  templateUrl: './dashboard-home.html',
+  styleUrl: './dashboard-home.scss'
+})
+export class DashboardHome {
+
+}
